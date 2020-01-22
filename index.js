dd162ac331c27592c38553df2b796d3ce12335be
Andrej Kocjancic <andrejkocjancic@bastion.test.os.int>
2020-01-22 16:13:35 +0100

    Added public and protected part

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/public', (req, res) => res.send('Public  World!'))
+app.get('/protected', (req, res) => res.send('Protected World!'))
+app.get('/', (req, res) => {
+  console.log(req);
+  res.send('Hello World!')
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
