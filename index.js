97330d418f6d83d66aca41b490aaed7bbd4f4851
Andrej Kocjancic <andrejkocjancic@bastion.test.os.int>
2020-01-22 16:21:18 +0100

    Chenged root response to JSON details

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ app.get('/public', (req, res) => res.send('Public  World!'))
 app.get('/protected', (req, res) => res.send('Protected World!'))
 app.get('/', (req, res) => {
   console.log(req);
-  res.send('Hello World!')
+  res.json({
+    "headers": req.headers,
+    "url": req.url,
+    "method": req.method,
+    "params": req.params,
+    "query": req.query
+  });
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
