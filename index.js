8f9f76108a817e64a745198fec30f6ff7bd22739
Andrej Kocjancic <andrejkocjancic@bastion.test.os.int>
2020-01-22 16:53:01 +0100

    Added cors, bodyParser, keycloak, express-session

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const express = require('express')
+const express = require('express');
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express()
 const port = 8080
 
+app.use(bodyParser.json());
+app.use(cors());
+
+var memoryStore = new session.MemoryStore();
+var keycloak = new Keycloak({ store: memoryStore });
+
+app.use(keycloak.middleware({
+  logout: '/logout',
+  admin: '/'
+}));
+
 app.get('/public', (req, res) => res.send('Public  World!'))
-app.get('/protected', (req, res) => res.send('Protected World!'))
+app.get('/protected', keycloak.protect(), (req, res) => res.send('Protected World!'))
 app.get('/', (req, res) => {
   console.log(req);
   res.json({
